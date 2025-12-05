6a40ce06a8b65443b9d7d3dd6250cfc569017a32
Alejandra_Calagua <alejandracalaguau@gmail.com>
2025-12-04 22:05:36 -0300

    detalle en contacto

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,14 +1,24 @@
 const Contacto = () => {
-  return(
+  return (
     <>
-    <div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+      <div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
 
-     <h2>Contacto</h2>
-     <h1 className="display-3 fw-bold"> Alejandra Calagua </h1>
-     <h3 className="fw-normal text-muted mb-3"> Entrega Final - React - TalentoTech </h3>
-    </div>
+        <h2>Contacto</h2>
+        <h1 className="display-3 fw-bold"> Alejandra Calagua </h1>
+        <h3 className="fw-normal text-muted mb-3"> Entrega Final - React - TalentoTech </h3>
+      <div className="ratio ratio-16x9">
+        <iframe
+        
+          src="https://www.youtube.com/embed/xL8Lot5xHyQ"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+        </div>
+      </div>
     </>
-   
+
   );
 }
 
